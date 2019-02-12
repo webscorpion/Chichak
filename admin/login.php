e19401d181d87bfcac1899e6c9ffeb1c5bca1c11
Siamak Rasouli <empire_of_web@yahoo.com>
2019-02-12 18:47:51 +0330

    admin temlate

---
+++ admin/login.php
@@ -0,0 +1,31 @@
+<body class="login-body">
+
+    <div class="container">
+
+      <form class="form-signin" action="index.html">
+        <h2 class="form-signin-heading">همین حالا وارد شوید</h2>
+        <div class="login-wrap">
+            <input type="text" class="form-control" placeholder="نام کاربری" autofocus>
+            <input type="password" class="form-control" placeholder="کلمه عبور">
+            <label class="checkbox">
+                <input type="checkbox" value="remember-me"> مرا به خاطر بسپار
+                <span class="pull-right"> <a href="#"> کلمه عبور را فراموش کرده اید؟</a></span>
+            </label>
+            <button class="btn btn-lg btn-login btn-block" type="submit">ورود</button>
+            <p>یا توسط یکی از حسابهای شبکه اجتماعی خود وارد شوید</p>
+            <div class="login-social-link">
+                <a href="index.html" class="facebook">
+                    <i class="icon-facebook"></i>
+                    Facebook
+                </a>
+                <a href="index.html" class="twitter">
+                    <i class="icon-twitter"></i>
+                    Twitter
+                </a>
+            </div>
+
+        </div>
+
+      </form>
+
+    </div>
