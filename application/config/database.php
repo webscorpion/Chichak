0cd85455ab1b94bcfa2875b8f7d637639a24ad78
Siamak Rasouli <empire_of_web@yahoo.com>
2019-02-12 18:15:51 +0330

    ignore test

--- application/config/database.php
+++ application/config/database.php
@@ -72,7 +72,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-
+asdasd
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'ipsd.ir:3306',
